776d2812688c5367c8781b8483aa5e2d2f3bc294
aravind1620 <aravind1620@gmail.com>
2025-03-23 21:41:53 +0530

    v3
    v3

--- lib/features/test_series/screens/ongoing_testseries.dart
+++ lib/features/test_series/screens/ongoing_testseries.dart
@@ -43,95 +43,108 @@ class _OngoingTestSeriesState extends State<OngoingTestSeries> {
             );
           }
 
-          List<Ongoing> testSeries = snapshot.data!.ongoing!;
-
-          return ListView.builder(
-            padding: const EdgeInsets.all(16),
-            itemCount: testSeries.length,
-            itemBuilder: (context, index) {
-              var test = testSeries[index];
-
-              return Container(
-                margin: const EdgeInsets.only(bottom: 12),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      AppConstant.secondaryColorLight,
-                      AppConstant.secondaryColorLight
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    const BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 6,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: ListTile(
-                  contentPadding: const EdgeInsets.all(12), // Reduced padding
-                  // isDense: true, // Helps prevent overflow
-                  leading: const Icon(Icons.assignment,
-                      size: 40, color: Colors.white),
-                  title: Flexible(
-                    child: Text(
-                      test.mainTestsName ?? "Unknown Test",
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Colors.white),
-                    ),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 8),
-                      _infoText(
-                          'Start Time',
-                          formatDate(test.mainTestsStart ?? ""),
-                          Colors.white70),
-                      _infoText('End Time', formatDate(test.mainTestsEnd ?? ""),
-                          Colors.white70),
-                      _infoText('Duration', test.mainTestsDuration ?? "N/A",
-                          Colors.white),
-                    ],
-                  ),
-                  trailing: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => StartQuizSeriesInfo(
-                            testid: test.mainTestsId ?? "0",
-                            quizTitle: test.mainTestsName ?? "Unknown Test",
-                            totalQuestions:
-                                int.tryParse(test.mainTestsQuestions ?? "0") ??
-                                    0,
-                            duration: test.mainTestsDuration ?? "N/A",
-                          ),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      foregroundColor: AppConstant.secondaryColorLight,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: const Text("Start"),
-                  ),
-                ),
-              );
-            },
-          );
+          return _buildTestSeriesList(snapshot.data!.ongoing!);
         },
       ),
+    );
+  }
+
+  Widget _buildTestSeriesList(List<Ongoing> testSeries) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: testSeries.length,
+      itemBuilder: (context, index) {
+        var test = testSeries[index];
+        return _buildTestSeriesItem(test);
+      },
+    );
+  }
+
+  Widget _buildTestSeriesItem(Ongoing test) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [AppConstant.primaryColor3, AppConstant.secondaryColorLight],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          const BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Text(
+                test.mainTestsName ?? "Unknown Test",
+                maxLines: 2,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading:
+                  const Icon(Icons.assignment, size: 40, color: Colors.white),
+              title: _buildTestSeriesInfo(test),
+              // subtitle: _buildTestSeriesInfo(test),
+              trailing: _buildStartButton(test),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTestSeriesInfo(Ongoing test) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 8),
+        _infoText('Start Time', formatDate(test.mainTestsStart ?? ""),
+            Colors.white70),
+        _infoText(
+            'End Time', formatDate(test.mainTestsEnd ?? ""), Colors.white70),
+        _DurationinfoText(
+            'Duration', test.mainTestsDuration ?? "N/A", Colors.white),
+      ],
+    );
+  }
+
+  Widget _buildStartButton(Ongoing test) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => StartQuizSeriesInfo(
+              testid: test.mainTestsId ?? "0",
+              quizTitle: test.mainTestsName ?? "Unknown Test",
+              totalQuestions: int.tryParse(test.mainTestsQuestions ?? "0") ?? 0,
+              duration: test.mainTestsDuration ?? "N/A",
+            ),
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: AppConstant.secondaryColorLight,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: const Text("Start"),
     );
   }
 
@@ -139,14 +152,43 @@ class _OngoingTestSeriesState extends State<OngoingTestSeries> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 2),
       child: Wrap(
-        crossAxisAlignment: WrapCrossAlignment.center,
+        alignment: WrapAlignment.start,
+        children: [
+          Icon(Icons.access_time, size: 14, color: textColor),
+          const SizedBox(width: 4),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "$label: ",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                value,
+                style: TextStyle(fontSize: 14, color: textColor),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _DurationinfoText(String label, String value, Color textColor) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Wrap(
+        alignment: WrapAlignment.start,
         children: [
           Icon(Icons.access_time, size: 14, color: textColor),
           const SizedBox(width: 4),
           Text(
             "$label: ",
             style: TextStyle(
-                fontSize: 14, fontWeight: FontWeight.bold, color: textColor),
+                fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),
           ),
           Text(
             value,
